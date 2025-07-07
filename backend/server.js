3e7023b5d6519c786f94d94298cef566c9d9cfe2
allan-bit-web <allankinuthia59@gmail.com>
2025-07-07 22:26:34 +0300

    corrected console.err to console.error

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ mongoose.connect(MONGO_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(() => console.log('MongoDB connected successfully!'))
-.catch(err => console.err('MongoDB error:',err));
+.catch(error => console.error('MongoDB error:',error));
 
 app.listen(PORT,() =>{
     console.log(`server is running on http://localhost:${PORT}`);
