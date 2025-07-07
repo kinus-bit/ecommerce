843e72d61e6aadd5bd457efeefbbaadd441f94da
allan-bit-web <allankinuthia59@gmail.com>
2025-07-07 22:29:59 +0300

    changed localhost to live backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from "react";
 import axios from 'axios';
 
-const API = 'http://localhost:5000/products';
+const API = 'https://ecommerce-first-otfc.onrender.com';
 
 export default function App(){
   const [products,setProducts] = useState([]);
