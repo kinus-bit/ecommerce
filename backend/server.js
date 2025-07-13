c1b9bd7ec34de4edb9ad1636eb682f2b3e44d208
allan-bit-web <allankinuthia59@gmail.com>
2025-07-13 14:21:43 +0300

    updated connection by removing unified parse

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,13 @@ const MONGO_URI = "mongodb://localhost:27017/productdb";
 const PORT = 6000;
 
 const app = express();
-app.use(cors({
-  origin: 'https://ecommerce-orcin-ten.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json())
 
 app.use('/products',productRoutes);
 
-mongoose.connect(MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(() => console.log('MongoDB connected successfully,well done!'))
+mongoose.connect(MONGO_URI)
+.then(() => console.log('MongoDB connected successfully,well done!'))
 .catch(error => console.error('MongoDB error:',error));
 
 app.get('/', (req, res) => {
