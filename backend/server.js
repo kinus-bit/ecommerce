1fc8ff91ce5151fef3e19ee7a36edc35f2cd6d4b
kinus-bit <mungaiallan540@gmail.com>
2025-09-26 21:35:48 +0300

    updated the server

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 
-//importing the route
+const express = require('express');
+const { connectDB } = require('./config/db')
+const cors = require('cors');
+// const dotenv =  require('dotenv');
 const productRoutes = require('./routes/productRoutes');
 
-//connecting to mongodb
-
-
+// dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json())
 
-app.use('/products',productRoutes);
+app.use('/api/products',productRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected successfully,well done!'))
-.catch(error => console.error('MongoDB error:',error));
+app.get('/',(req,res) =>{
+    res.send('backend running');
+})
 
+connectDB();
 
-const PORT = process.env.PORT || 6000;
+// const PORT = process.env.MONGO_URI || 5000;
+const PORT = 5000;
+
 
 app.listen(PORT,() =>{
     console.log(`server is running on http://localhost:${PORT}`);
