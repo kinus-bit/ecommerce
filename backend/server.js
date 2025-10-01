54b4f1b43b16bfde98bfc5e508d0c2bcefb2c28a
kinus-bit <mungaiallan540@gmail.com>
2025-10-01 23:33:20 +0300

    updated env

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,25 @@
+const express = require("express");
+const { connectDB } = require("./config/db");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const productRoutes = require("./routes/productRoutes");
 
-const express = require('express');
-const { connectDB } = require('./config/db')
-const cors = require('cors');
-// const dotenv =  require('dotenv');
-const productRoutes = require('./routes/productRoutes');
-
-// dotenv.config();
+dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/products',productRoutes);
+app.use("/api/products", productRoutes);
 
-app.get('/',(req,res) =>{
-    res.send('backend running');
-})
+app.get("/", (req, res) => {
+  res.send("backend running");
+});
 
 connectDB();
 
-// const PORT = process.env.MONGO_URI || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT,() =>{
-    console.log(`server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
 });
-
-
-
