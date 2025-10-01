4a6fcc030b28a60a40b0e982fdf7c37912ee9327
kinus-bit <mungaiallan540@gmail.com>
2025-10-01 23:33:42 +0300

    added database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,14 @@
 const mongoose = require('mongoose');
 
-// exports.connectDB = async () =>{
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log("Mongodb connected successfully");
-        
-//     } catch (error) {
-//         console.error("Mongodb connection failed:",error.message); 
-//         process.exit(1);
-//     }
-// }
-
-let MONGO_URI = "mongodb://localhost:27017/productdb";
-
 exports.connectDB = async () =>{
     try {
-        await mongoose.connect(MONGO_URI);
-        console.log("Mongodb connected successfully");
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Mongodb connected successfully!!!!");
         
     } catch (error) {
         console.error("Mongodb connection failed:",error.message); 
         process.exit(1);
     }
 }
+
 
