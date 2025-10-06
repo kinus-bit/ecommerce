085aa141bd7ea547d568c110a1088469596a206f
kinus-bit <mungaiallan540@gmail.com>
2025-10-06 22:48:45 +0300

    added protected routes and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,56 @@ import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
 import FeedbackPage from "./pages/Feedback";
 import ContactPage from "./pages/Contact";
-import Navigation from "./components/Navigation";
-import Footer from "@/components/Footer"
+import Footer from "@/components/Footer";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProtectedRoute from './utils/ProtectedRoute';
+import RoleDashboard from './pages/RoleDashboard';
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import AdminOrder from "./pages/admin/order"
 import AdminProduct from "./pages/admin/product"
-import ProductDasbboard from "./pages/ProductDasbboard"
 
 export default function App() {
   return (
     <div >
-      <Navigation />
+
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/About' element={<AboutPage />} />
         <Route path='/feedback' element={<FeedbackPage />} />
         <Route path='/contact' element={<ContactPage />} />
-        <Route path='/admin' element={<AdminDashboard />} />
-        <Route path='/admin/order' element={<AdminOrder />} />
-        <Route path='/admin/product' element={<AdminProduct />} />
-        <Route path='/dashboard' element={<ProductDasbboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <RoleDashboard />
+            </ProtectedRoute>}
+        />
+        <Route path='/adminhome'
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+
+          } />
+        <Route path='/order'
+          element={
+            <ProtectedRoute>
+              <AdminOrder />
+            </ProtectedRoute>
+
+          } />
+        <Route path='/product'
+          element={
+            <ProtectedRoute>
+              <AdminProduct />
+            </ProtectedRoute>
+
+          } />
       </Routes>
-      <Footer className="justify-center items-center"/>
+      <Footer className="justify-center items-center" />
     </div>
   );
 }
