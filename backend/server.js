5d3089af89be10443b0f37f9ebb8ae33bff559d1
kinus-bit <mungaiallan540@gmail.com>
2025-10-06 22:47:29 +0300

    added user route

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { connectDB } = require("./config/db");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const productRoutes = require("./routes/productRoutes");
-
+const userRoutes = require("./routes/userRoutes")
 dotenv.config();
 const app = express();
 
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api/users",userRoutes);
 
 app.get("/", (req, res) => {
   res.send("backend running");
